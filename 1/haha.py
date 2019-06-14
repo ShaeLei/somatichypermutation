d2e2e5c8fe0ad666f344ac5ee0f0b2db8f5d97a8
Wei Tian <wtian7@sts-lianglab.dhcp.uic.edu>
2019-06-14 17:49:35 -0500

    3,4

--- 1/haha.py
+++ 1/haha.py
@@ -1,5 +1,5 @@
 import glob
 import os
-for fn in glob.glob('*_cmpr'):
-    nfn = fn.replace('_cmpr','.cmpr')
+for fn in glob.glob('*_collapse'):
+    nfn = fn.replace('_collapse','.collapse')
     os.system('mv '+fn+' '+nfn)
